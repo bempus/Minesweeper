1c19efeca75930ddeb8aec85613e83e20aad73e1
Jonathan Svahn <bempus@q-er.live>
2024-08-24 15:56:55 +0200

    Changed to dynamic port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static(__dirname + "/static"));
+app.use(express.static("/static"));
 
-app.listen(80);
+app.listen(process.env.PORT || 3000);
